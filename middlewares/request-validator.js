f1c958e4a991e28f0de5a4e716bdc955e4299db7
Alex Strelin <alexstrelin@yandex.ru>
2023-09-21 13:48:20 +0300

    fix: создать кастомный метод для валидации ссылок

--- middlewares/request-validator.js
+++ middlewares/request-validator.js
@@ -1,5 +1,12 @@
 const { celebrate, Joi } = require('celebrate');
+const isURL = require('validator/lib/isURL');
 const messages = require('../utils/constants').validateMessages;
+
+const url = (value, helpers) => (
+  isURL(value)
+    ? value
+    : helpers.error('string.urlCustomMethod')
+);
 
 const validateUser = celebrate({
   body: Joi.object().keys({
@@ -32,9 +39,9 @@ const validateMovie = celebrate({
     duration: Joi.number().required().messages(messages),
     year: Joi.string().required().messages(messages),
     description: Joi.string().required().messages(messages),
-    image: Joi.string().required().uri({ scheme: /https?/ }).messages(messages),
-    trailerLink: Joi.string().required().uri({ scheme: /https?/ }).messages(messages),
-    thumbnail: Joi.string().required().uri({ scheme: /https?/ }).messages(messages),
+    image: Joi.string().required().custom(url).messages(messages),
+    trailerLink: Joi.string().required().custom(url).messages(messages),
+    thumbnail: Joi.string().required().custom(url).messages(messages),
     movieId: Joi.number().required().messages(messages),
     nameRU: Joi.string().required().messages(messages),
     nameEN: Joi.string().required().messages(messages),
